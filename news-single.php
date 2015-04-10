c07b95340483514d39b76b5058db8d9d16839935
danielocampo223 <philwebservices.daniel@gmail.com>
2015-04-10 17:14:23 +0800

    create remaining subpages

---
+++ news-single.php
@@ -0,0 +1,31 @@
+ <?php include('includes/header.php'); ?>
+ 
+  <div class="content content-news">
+    <div class="container">
+      <h1 class="page-title">News</h1>
+      <div class="row post-news">
+        <div class="col-md-12">
+          <div class="post-block post-block-news">
+            <div class="post-head">
+              <h3 class="post-title">Alberta IT Partners Up with Two Popular Sydney Hotspots</h3>
+              <p class="post-news-date">Posted on: Monday, February 16th, 2015</p>
+            </div>
+            <div class="post-body">
+              <div class="post-block-image">
+                <img class="img-responsive" src="images/news-single.jpg" alt="image">
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa...</p>
+              <a class="btn btn-primary">Read More</a>
+              <ul class="social">
+                <li><a href="#" class="btn btn-linkedin"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="#" class="btn btn-facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#" class="btn btn-twitter"><i class="fa fa-twitter"></i></a></li>
+              </ul>
+            </div>
+          </div><!-- /.post-block -->
+        </div>
+      </div>
+    </div><!-- /.container -->
+  </div><!-- /.content-news -->
+  
+<?php include('includes/footer.php'); ?>
--- news.php
+++ news.php
@@ -3,8 +3,8 @@
   <div class="content content-news">
     <div class="container">
       <h1 class="page-title">News</h1>
-      <div class="row">
-        <div class="col-md-4">
+      <div class="row post-news">
+        <div class="col-sm-6 col-md-4">
           <div class="post-block post-block-news">
             <div class="post-head">
               <h3 class="post-title">Alberta IT Partners Up with Two Popular Sydney Hotspots</h3>
@@ -15,11 +15,16 @@
                 <img class="img-responsive" src="images/servers.jpg" alt="image">
               </div>
               <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa...</p>
-              <div class="btn btn-primary">Read More</div>
+              <a class="btn btn-primary">Read More</a>
+              <ul class="social">
+                <li><a href="#" class="btn btn-linkedin"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="#" class="btn btn-facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#" class="btn btn-twitter"><i class="fa fa-twitter"></i></a></li>
+              </ul>
             </div>
           </div><!-- /.post-block -->
         </div>
-        <div class="col-md-4">
+        <div class="col-sm-6 col-md-4">
           <div class="post-block post-block-news">
             <div class="post-head">
               <h3 class="post-title">Alberta IT Partners Up with Two Popular Sydney Hotspots</h3>
@@ -30,11 +35,16 @@
                 <img class="img-responsive" src="images/christmas.jpg" alt="image">
               </div>
               <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa...</p>
-              <div class="btn btn-primary">Read More</div>
+              <a class="btn btn-primary">Read More</a>
+              <ul class="social">
+                <li><a href="#" class="btn btn-linkedin"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="#" class="btn btn-facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#" class="btn btn-twitter"><i class="fa fa-twitter"></i></a></li>
+              </ul>
             </div>
           </div><!-- /.post-block -->
         </div>
-        <div class="col-md-4">
+        <div class="col-sm-6 col-md-4">
           <div class="post-block post-block-news">
             <div class="post-head">
               <h3 class="post-title">Alberta IT Partners Up with Two Popular Sydney Hotspots</h3>
@@ -45,11 +55,16 @@
                 <img class="img-responsive" src="images/trees.jpg" alt="image">
               </div>
               <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa...</p>
-              <div class="btn btn-primary">Read More</div>
+              <a class="btn btn-primary">Read More</a>
+              <ul class="social">
+                <li><a href="#" class="btn btn-linkedin"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="#" class="btn btn-facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#" class="btn btn-twitter"><i class="fa fa-twitter"></i></a></li>
+              </ul>
             </div>
           </div><!-- /.post-block -->
         </div>
-        <div class="col-md-4">
+        <div class="col-sm-6 col-md-4">
           <div class="post-block post-block-news">
             <div class="post-head">
               <h3 class="post-title">Alberta IT Partners Up with Two Popular Sydney Hotspots</h3>
@@ -60,11 +75,16 @@
                 <img class="img-responsive" src="images/mountain.jpg" alt="image">
               </div>
               <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa...</p>
-              <div class="btn btn-primary">Read More</div>
+              <a class="btn btn-primary">Read More</a>
+              <ul class="social">
+                <li><a href="#" class="btn btn-linkedin"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="#" class="btn btn-facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#" class="btn btn-twitter"><i class="fa fa-twitter"></i></a></li>
+              </ul>
             </div>
           </div><!-- /.post-block -->
         </div>
-        <div class="col-md-4">
+        <div class="col-sm-6 col-md-4">
           <div class="post-block post-block-news">
             <div class="post-head">
               <h3 class="post-title">Alberta IT Partners Up with Two Popular Sydney Hotspots</h3>
@@ -75,11 +95,16 @@
                 <img class="img-responsive" src="images/road.jpg" alt="image">
               </div>
               <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa...</p>
-              <div class="btn btn-primary">Read More</div>
+              <a class="btn btn-primary">Read More</a>
+              <ul class="social">
+                <li><a href="#" class="btn btn-linkedin"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="#" class="btn btn-facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#" class="btn btn-twitter"><i class="fa fa-twitter"></i></a></li>
+              </ul>
             </div>
           </div><!-- /.post-block -->
         </div>
-        <div class="col-md-4">
+        <div class="col-sm-6 col-md-4">
           <div class="post-block post-block-news">
             <div class="post-head">
               <h3 class="post-title">Alberta IT Partners Up with Two Popular Sydney Hotspots</h3>
@@ -90,11 +115,22 @@
                 <img class="img-responsive" src="images/sea.jpg" alt="image">
               </div>
               <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa...</p>
-              <div class="btn btn-primary">Read More</div>
+              <a class="btn btn-primary">Read More</a>
+              <ul class="social">
+                <li><a href="#" class="btn btn-linkedin"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="#" class="btn btn-facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#" class="btn btn-twitter"><i class="fa fa-twitter"></i></a></li>
+              </ul>
             </div>
           </div><!-- /.post-block -->
         </div>
       </div>
+
+      <ul class="pagination">
+        <li><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+      </ul>
     </div><!-- /.container -->
   </div><!-- /.content-news -->
   
